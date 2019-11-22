3c8d4b29251dda5ecd9e48f73da8f7a5d3aeca80
Maciej Jarosiewicz <mjarosie@users.noreply.github.com>
2019-11-22 14:23:27 +0000

    implement excercises 1.3 and 1.4

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -1,7 +1,24 @@
 module Chapter1
     ( divides
+    , ldf
+    , ldf'
     ) where
 
 divides :: Integer -> Integer -> Bool
 divides d n = rem n d == 0
+
+ld :: Integer -> Integer
+ld n = ldf 2 n
+
+ldf :: Integer -> Integer -> Integer
+ldf k n | divides k n   = k
+        | k^2 > n       = n
+        | otherwise     = ldf (k+1) n
+
+ldf' :: Integer -> Integer -> Integer
+ldf' k n
+    | divides k n   = k
+    | k^2 >= n      = n
+    | otherwise     = ldf (k+1) n
+
 
--- test/Chapter1Spec.hs
+++ test/Chapter1Spec.hs
@@ -1,8 +1,25 @@
 import Test.Hspec
 import Chapter1
+import Control.Exception
 
 main :: IO ()
 main = hspec $ do
     describe "divides" $ do 
         it "returns True when the first number divides the second" $
-           2 `divides` 4 `shouldBe` True 
+            2 `divides` 4 `shouldBe` True 
+        it "returns False when the first number doesn't divide the second" $
+            2 `divides` 5 `shouldBe` False 
+        it "throws an exception when the first number is 0" $
+            evaluate (0 `divides` 2) `shouldThrow` anyArithException 
+        it "returns True when the second number is 0" $
+            2 `divides` 0 `shouldBe` True 
+    describe "lowest discriminator" $ do
+        it "returns the lowest discriminator of 2 and 4" $
+            ldf 2 4 `shouldBe` 2
+        it "returns the lowest discriminator of 2 and 0" $
+            ldf 2 0 `shouldBe` 2
+        it "throws an exception when the second argument is 0" $
+            evaluate (ldf 0 2) `shouldThrow` anyArithException
+    describe "lowest discriminator alternative" $ do
+        it "returns the same result for edge case as the equality doesn't make difference" $
+            ldf 2 4 `shouldBe` ldf' 2 4
